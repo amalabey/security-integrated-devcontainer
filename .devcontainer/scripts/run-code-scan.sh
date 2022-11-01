39ba479464b46a0a18f321240412e7abd60dea3e
Amal Abeygunawardana <amalabey@gmail.com>
2022-11-01 12:37:37 +0000

    Owasp dep check works against dlls and csprojs

--- .devcontainer/scripts/run-code-scan.sh
+++ .devcontainer/scripts/run-code-scan.sh
@@ -16,19 +16,21 @@ else
     
     echo "Running horusec security scanner"
     horusec start -p /workspace -P $HOST_PROJECT_PATH --config-file-path=/workspace/.devcontainer/horusec-config.json -o="sonarqube" -O="$HORUSEC_ISSUES_FILE" --log-level=debug
-    # sanitise horusec output to make it more sonarqube compatible
-    horusec_output=$(cat $HORUSEC_ISSUES_FILE |  jq '.issues[].primaryLocation.filePath |= "/workspace/"+.' | jq '.issues[].primaryLocation.message |= sub("^.+detected:\\s"; "")')
+    sanitise horusec output to make it more sonarqube compatible
+    horusec_output=$(cat $HORUSEC_ISSUES_FILE | \
+         jq '.issues[].primaryLocation.filePath |= "/workspace/"+.' \
+         | jq '.issues[].primaryLocation.message |= sub("^.+detected:\\s"; "")')
     echo $horusec_output > $HORUSEC_ISSUES_FILE
-    vuln_dlls=$(jq '.issues[].primaryLocation.filePath' security-issues.json | tr -d '"' | grep ".dll$" | tr '\n' ',')
+    vuln_dlls=$(jq '.issues[].primaryLocation.filePath' security-issues.json | tr -d '"' | grep -E ".(dll|csproj)$" | uniq | tr '\n' ',')
     
     echo "SQ: Running sonar scan"
     dotnet sonarscanner begin /k:$SQ_PROJECT_KEY /d:sonar.login=$SQ_AUTH_TOKEN /d:sonar.host.url=http://sonarqube:9000 \
     /d:sonar.cs.vscoveragexml.reportsPaths=coverage.xml \
     /d:sonar.verbose=true \
     /d:sonar.scm.exclusions.disabled=true \
-    /d:sonar.externalIssuesReportPaths=/workspace/$HORUSEC_ISSUES_FILE \
     /d:sonar.projectBaseDir=/workspace \
     /d:sonar.javascript.exclusions="node_modules" \
+    /d:sonar.externalIssuesReportPaths=/workspace/$HORUSEC_ISSUES_FILE \
     /d:sonar.sources=$vuln_dlls
 
     dotnet build
@@ -37,18 +39,18 @@ else
     dotnet sonarscanner end /d:sonar.login=$SQ_AUTH_TOKEN
     echo "SQ: Done. SonarScan completed"
 
-#     echo "DT: Running depedency scan"
-#     repo_name=$(basename -s .git `git config --get remote.origin.url`)
-#     sln_path=$(find /workspace -name *.sln)
-#     dotnet CycloneDX $sln_path -o /tmp
-#     curl -X "POST" "http://dtrack-apiserver:8080/api/v1/bom" \
-#      -H 'Content-Type: multipart/form-data' \
-#      -H "X-Api-Key: $DT_AUTH_TOKEN" \
-#      -F "autoCreate=true" \
-#      -F "projectName=$repo_name" \
-#      -F "projectVersion=1" \
-#      -F "bom=@/tmp/bom.xml"
+    echo "DT: Running depedency scan"
+    repo_name=$(basename -s .git `git config --get remote.origin.url`)
+    sln_path=$(find /workspace -name *.sln)
+    dotnet CycloneDX $sln_path -o /tmp
+    curl -X "POST" "http://dtrack-apiserver:8080/api/v1/bom" \
+     -H 'Content-Type: multipart/form-data' \
+     -H "X-Api-Key: $DT_AUTH_TOKEN" \
+     -F "autoCreate=true" \
+     -F "projectName=$repo_name" \
+     -F "projectVersion=1" \
+     -F "bom=@/tmp/bom.xml"
 
-#     # echo "DT: Done. Dependency scan completed"
+    # echo "DT: Done. Dependency scan completed"
 fi
 
