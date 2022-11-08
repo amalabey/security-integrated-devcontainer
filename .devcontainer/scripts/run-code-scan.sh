4b392a33b3741c5f4b98a8271787d625be48d7af
Amal Abeygunawardana <amalabey@gmail.com>
2022-11-08 04:38:50 +0000

    Use sarif from ext scan base dir

--- .devcontainer/scripts/run-code-scan.sh
+++ .devcontainer/scripts/run-code-scan.sh
@@ -1,35 +1,31 @@
 #!/bin/bash
 
-ENV_VAR_FILE=/workspace/.devcontainer/local.env
-
-workingDir=$1
-cd $workingDir
-cwd=$(pwd)
-echo "Current working directory: $workingDir"
+WORKING_DIR=/workspace
+ENV_VAR_FILE=$WORKING_DIR/.devcontainer/local.env
 
 if [ ! -f $ENV_VAR_FILE ]
 then
     echo "SQ: Environment config not found. Skipping code scan"
 else
     export $(cat $ENV_VAR_FILE | xargs) >/dev/null
-    
-    
+
+
     echo "SQ: Running sonar scan"
     dotnet sonarscanner begin /k:$SQ_PROJECT_KEY /d:sonar.login=$SQ_AUTH_TOKEN /d:sonar.host.url=http://sonarqube:9000 \
     /d:sonar.verbose=true \
     /d:sonar.scm.exclusions.disabled=true \
-    /d:sonar.projectBaseDir=/workspace \
+    /d:sonar.projectBaseDir=$WORKING_DIR \
     /d:sonar.javascript.exclusions="node_modules" \
-    /d:sonar.dependencyCheck.jsonReportPath="/workspace/.devcontainer/dependency-check-report.json" \
-    /d:sonar.dependencyCheck.htmlReportPath="/workspace/.devcontainer/dependency-check-report.html" \
-    /d:sonar.sarif.path=/workspace/.devcontainer/horusec-results.sarif,/workspace/.devcontainer/results_sarif.sarif
+    /d:sonar.dependencyCheck.jsonReportPath="$WORKING_DIR/.devcontainer/dependency-check-report.json" \
+    /d:sonar.dependencyCheck.htmlReportPath="$WORKING_DIR/.devcontainer/dependency-check-report.html" \
+    /d:sonar.sarif.path=$WORKING_DIR/src/horusec-results.sarif,$WORKING_DIR/src/results_sarif.sarif
 
     dotnet build
     dotnet-coverage collect 'dotnet test' -f xml  -o 'coverage.xml'
 
-    horusec start -D -p /workspace/src -P $HOST_PROJECT_PATH --config-file-path=/workspace/.devcontainer/horusec-config.json -o="sarif" -O="/workspace/.devcontainer/horusec-results.sarif" --log-level=debug
-    checkov -d /workspace/src -o sarif --output-file-path=/workspace/.devcontainer
-    /usr/bin/dependency-check.sh -f JSON -f HTML -s . -o .devcontainer --disableAssembly
+    horusec start -D -p $WORKING_DIR/src -P $HOST_PROJECT_PATH --config-file-path=$WORKING_DIR/.devcontainer/horusec-config.json -o="sarif" -O="$WORKING_DIR/src/horusec-results.sarif" --log-level=debug
+    checkov -d $WORKING_DIR/src -o sarif --output-file-path=$WORKING_DIR/src
+    /usr/bin/dependency-check.sh -f JSON -f HTML -s src -o .devcontainer --disableAssembly --log=dep-check.log
 
     dotnet sonarscanner end /d:sonar.login=$SQ_AUTH_TOKEN
     echo "SQ: Done. SonarScan completed"
